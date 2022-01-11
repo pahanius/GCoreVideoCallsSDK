0af7f9a4778206619b2964376842491c9039c61d
Pavel Volobuev <p.volobuev@l-tech.ru>
2022-01-11 13:23:20 +0300

    WIP

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ import PackageDescription
 let package = Package(
   name: "GCoreVideoCallsSDK",
   platforms: [
-    .iOS(.v12)
+    .macOS(.v10_15), .iOS(.v12), .tvOS(.v12)
   ],
   products: [
     .library(
